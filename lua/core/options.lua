9af8ff5dc27890ba42670f7c45cb7ad816fb8fff
Arun Tomar <tomar.arun@gmail.com>
2024-06-12 00:07:41 -0400

    add more plugins

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -11,8 +11,8 @@ opt.expandtab = true -- expand tab to spaces
 opt.autoindent = true -- copy indent from current line when starting new one
 
 -- Sets how neovim will display certain whitespace characters in the editor
-opt.list = true
-opt.listchars = { tab = ">> ", trail = ".", nbsp = "_" }
+-- opt.list = true
+-- opt.listchars = { tab = ">> ", trail = ".", nbsp = "_" }
 
 -- line wrapping
 opt.wrap = false -- disable line wrapping
