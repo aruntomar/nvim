d79339646f3f3b52f99687bdb101ce6d3145e98a
Arun Tomar <arun@automatehub.com>
2024-08-17 20:30:24 -0400

    configure go language server for go development

--- lua/plugins/lsp/lspconfig.lua
+++ lua/plugins/lsp/lspconfig.lua
@@ -9,6 +9,7 @@ return {
 	config = function()
 		-- import lspconfig plugin
 		local lspconfig = require("lspconfig")
+		local util = require("lspconfig/util")
 
 		-- import mason_lspconfig plugin
 		local mason_lspconfig = require("mason-lspconfig")
@@ -146,6 +147,24 @@ return {
 			capabilities = capabilities,
 			on_attach = on_attach,
 			filetypes = { "terraform" },
+		})
+
+		--configure gopls language server
+		lspconfig["gopls"].setup({
+			capabilities = capabilities,
+			on_attach = on_attach,
+			cmd = { "gopls" },
+			filetypes = { "go", "gomod", "gowork", "gotmpl" },
+			root_dir = util.root_pattern("go.work", "go.mod", ".git"),
+			settings = {
+				gopls = {
+					completeUnimported = true,
+					usePlaceholders = true,
+					analyses = {
+						unusedparams = true,
+					},
+				},
+			},
 		})
 
 		-- configure lua server (with special settings)
